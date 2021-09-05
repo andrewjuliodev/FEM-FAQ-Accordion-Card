b8a7848bd47f3b31c986634e08efc382db21fe0e
Andrew JT <andrew.julio.tompsett@gmail.com>
2021-09-05 17:14:42 +0200

    Logic to only have one Answer open at a time!
    Managed to complete the logic to have only one Answer show at any one time, as otherwise the FAQ would go outside of its borders!

--- js/script.js
+++ js/script.js
@@ -6,9 +6,18 @@
 const accordion = document.getElementsByClassName("container");
 const container = document.querySelectorAll(".container");
 
+function faq() {}
+
 for (let i = 1; i < accordion.length + 1; i++) {
   document.querySelector(`.q${i}`).addEventListener("click", function () {
     if (document.querySelector(`.cont${i}`).style.height === "55px") {
+      for (let i = 1; i < accordion.length + 1; i++) {
+        if (document.querySelector(`.cont${i}`).style.height === "100px") {
+          document.querySelector(`.arrow${i}`).style.transform = "scaleY(1)";
+          document.querySelector(`.cont${i}`).style.height = "55px";
+          document.querySelector(`.a${i}`).style.display = "none";
+        }
+      }
       document.querySelector(`.arrow${i}`).style.transform = "scaleY(-1)";
       document.querySelector(`.cont${i}`).style.height = "100px";
       document.querySelector(`.a${i}`).style.display = "block";
@@ -23,6 +32,13 @@ for (let i = 1; i < accordion.length + 1; i++) {
 for (let i = 1; i < accordion.length + 1; i++) {
   document.querySelector(`.arrow${i}`).addEventListener("click", function () {
     if (document.querySelector(`.cont${i}`).style.height === "55px") {
+      for (let i = 1; i < accordion.length + 1; i++) {
+        if (document.querySelector(`.cont${i}`).style.height === "100px") {
+          document.querySelector(`.arrow${i}`).style.transform = "scaleY(1)";
+          document.querySelector(`.cont${i}`).style.height = "55px";
+          document.querySelector(`.a${i}`).style.display = "none";
+        }
+      }
       document.querySelector(`.arrow${i}`).style.transform = "scaleY(-1)";
       document.querySelector(`.cont${i}`).style.height = "100px";
       document.querySelector(`.a${i}`).style.display = "block";
