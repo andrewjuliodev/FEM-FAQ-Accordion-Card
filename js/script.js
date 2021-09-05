cbc75616195a6e612e4ecc6c8fd4006ad4ea07e7
Andrew JT <andrew.julio.tompsett@gmail.com>
2021-09-05 17:43:12 +0200

    Refactored
    Refactored by listening to any click of the parent Div of the Question & Arrow, thus halfing the JS code!

--- js/script.js
+++ js/script.js
@@ -8,28 +8,7 @@ const accordion = document.getElementsByClassName("container");
 function faq() {}
 
 for (let i = 1; i < accordion.length + 1; i++) {
-  document.querySelector(`.q${i}`).addEventListener("click", function () {
-    if (document.querySelector(`.cont${i}`).style.height === "55px") {
-      for (let i = 1; i < accordion.length + 1; i++) {
-        if (document.querySelector(`.cont${i}`).style.height === "100px") {
-          document.querySelector(`.arrow${i}`).style.transform = "scaleY(1)";
-          document.querySelector(`.cont${i}`).style.height = "55px";
-          document.querySelector(`.a${i}`).style.display = "none";
-        }
-      }
-      document.querySelector(`.arrow${i}`).style.transform = "scaleY(-1)";
-      document.querySelector(`.cont${i}`).style.height = "100px";
-      document.querySelector(`.a${i}`).style.display = "block";
-    } else {
-      document.querySelector(`.arrow${i}`).style.transform = "scaleY(1)";
-      document.querySelector(`.cont${i}`).style.height = "55px";
-      document.querySelector(`.a${i}`).style.display = "none";
-    }
-  });
-}
-
-for (let i = 1; i < accordion.length + 1; i++) {
-  document.querySelector(`.arrow${i}`).addEventListener("click", function () {
+  document.querySelector(`.row${i}`).addEventListener("click", function () {
     if (document.querySelector(`.cont${i}`).style.height === "55px") {
       for (let i = 1; i < accordion.length + 1; i++) {
         if (document.querySelector(`.cont${i}`).style.height === "100px") {
