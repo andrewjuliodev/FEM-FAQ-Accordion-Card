db60c0a00a00d1398bfac2d5ceb0d8d7b9f2a619
Andrew JT <andrew.julio.tompsett@gmail.com>
2021-09-05 16:35:58 +0200

    Working on the Accordion Logic

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// accordion        accordion
+// container        contentBx
+// questionRow      label
+// answer           content
+
+const accordion = document.getElementsByClassName("container");
+
+for (let i = 1; i < accordion.length + 1; i++) {
+  document.querySelector(`.q${i}`).addEventListener("click", function () {
+    if (document.querySelector(`.cont${i}`).style.height === "55px") {
+      document.querySelector(`.arrow${i}`).style.transform = "scaleY(-1)";
+      document.querySelector(`.cont${i}`).style.height = "100px";
+      document.querySelector(`.a${i}`).style.display = "block";
+    } else {
+      document.querySelector(`.arrow${i}`).style.transform = "scaleY(1)";
+      document.querySelector(`.cont${i}`).style.height = "55px";
+      document.querySelector(`.a${i}`).style.display = "none";
+    }
+  });
+}
+
+// for (let i = 1; i < accordion.length + 1; i++) {
+//   document.querySelector(`.arrow${i}`).addEventListener("click", function () {
+//     if (document.querySelector(`.cont${i}`).style.height === "55px") {
+//       document.querySelector(`.arrow${i}`).style.transform = "scaleY(-1)";
+//       document.querySelector(`.cont${i}`).style.height = "100px";
+//       document.querySelector(`.a${i}`).style.display = "block";
+//     } else {
+//       document.querySelector(`.arrow${i}`).style.transform = "scaleY(1)";
+//       document.querySelector(`.cont${i}`).style.height = "55px";
+//       document.querySelector(`.a${i}`).style.display = "none";
+//     }
+//   });
+// }
