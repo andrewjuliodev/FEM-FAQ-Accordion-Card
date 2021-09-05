348b3d6c6c7e10a3f12773a2eb7113b31e5c7b10
Andrew JT <andrew.julio.tompsett@gmail.com>
2021-09-05 16:51:36 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 // answer           content
 
 const accordion = document.getElementsByClassName("container");
+const container = document.querySelectorAll(".container");
 
 for (let i = 1; i < accordion.length + 1; i++) {
   document.querySelector(`.q${i}`).addEventListener("click", function () {
@@ -19,16 +20,16 @@ for (let i = 1; i < accordion.length + 1; i++) {
   });
 }
 
-// for (let i = 1; i < accordion.length + 1; i++) {
-//   document.querySelector(`.arrow${i}`).addEventListener("click", function () {
-//     if (document.querySelector(`.cont${i}`).style.height === "55px") {
-//       document.querySelector(`.arrow${i}`).style.transform = "scaleY(-1)";
-//       document.querySelector(`.cont${i}`).style.height = "100px";
-//       document.querySelector(`.a${i}`).style.display = "block";
-//     } else {
-//       document.querySelector(`.arrow${i}`).style.transform = "scaleY(1)";
-//       document.querySelector(`.cont${i}`).style.height = "55px";
-//       document.querySelector(`.a${i}`).style.display = "none";
-//     }
-//   });
-// }
+for (let i = 1; i < accordion.length + 1; i++) {
+  document.querySelector(`.arrow${i}`).addEventListener("click", function () {
+    if (document.querySelector(`.cont${i}`).style.height === "55px") {
+      document.querySelector(`.arrow${i}`).style.transform = "scaleY(-1)";
+      document.querySelector(`.cont${i}`).style.height = "100px";
+      document.querySelector(`.a${i}`).style.display = "block";
+    } else {
+      document.querySelector(`.arrow${i}`).style.transform = "scaleY(1)";
+      document.querySelector(`.cont${i}`).style.height = "55px";
+      document.querySelector(`.a${i}`).style.display = "none";
+    }
+  });
+}
